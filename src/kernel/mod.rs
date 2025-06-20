b514fff08d71c6cebd5f922914ac99eb31b33e30
devil <europedreadlydevil@gmail.com>
2025-06-20 15:25:44 +0300

    Map a page and write to it for testing purposes
    This commit introduces a function to map a specific page in memory and write a value to it. This is for testing the memory management functionality of the kernel. An empty frame allocator is used as a temporary solution.

--- src/kernel/mod.rs
+++ src/kernel/mod.rs
@@ -1,10 +1,11 @@
 use bootloader::BootInfo;
-use x86_64::structures::paging::{OffsetPageTable, Translate};
+use x86_64::structures::paging::{OffsetPageTable, Page, Translate};
 use x86_64::{VirtAddr};
 #[allow(unused)]
 use crate::{gdt, println};
 use crate::interrupts::{idt, pic};
 use crate::{debug_call, memory};
+use crate::memory::{create_example_mapping, EmptyFrameAllocator};
 
 pub(crate) mod debug;
 
@@ -34,27 +35,17 @@ impl Kernel {
     }
     
     #[cfg(not(feature = "test"))]
-    pub fn run(self) -> ! {
+    pub fn run(mut self) -> ! {
 
         let boot_info = self.boot_info;
-        
 
-        let addresses = [
-            // the identity-mapped vga buffer page
-            0xb8000,
-            // some code page
-            0x201008,
-            // some stack page
-            0x0100_0020_1a10,
-            // virtual address mapped to physical address 0
-            boot_info.physical_memory_offset,
-        ];
+        let mut frame_alloc = EmptyFrameAllocator;
 
-        for &address in &addresses {
-            let virt = VirtAddr::new(address);
-            let phys = self.mapper.translate_addr(virt);
-            println!("{:?} -> {:?}", virt, phys);
-        }
+        let page = Page::containing_address(VirtAddr::new(0));
+        create_example_mapping(page, &mut self.mapper, &mut frame_alloc);
+
+        let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+        unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e) }
 
         #[allow(clippy::empty_loop)]
         loop {
--- src/memory/mod.rs
+++ src/memory/mod.rs
@@ -1,5 +1,5 @@
 use x86_64::registers::control::Cr3;
-use x86_64::structures::paging::{OffsetPageTable, PageTable};
+use x86_64::structures::paging::{FrameAllocator, Mapper, OffsetPageTable, Page, PageTable, PageTableFlags, PhysFrame, Size4KiB};
 use x86_64::{PhysAddr, VirtAddr};
 use x86_64::structures::paging::page_table::FrameError;
 
@@ -32,4 +32,28 @@ pub unsafe fn active_level_4_table(physical_memory_offset: VirtAddr)
     let virt = physical_memory_offset + phys.as_u64();
     let page_table_ptr: *mut PageTable = virt.as_mut_ptr();
     unsafe { &mut *page_table_ptr }
+}
+
+pub fn create_example_mapping(
+    page: Page,
+    mapper: &mut OffsetPageTable,
+    frame_allocator: &mut impl FrameAllocator<Size4KiB>,
+) {
+    let frame = PhysFrame::containing_address(PhysAddr::new(0xb8000));
+    let flags = PageTableFlags::PRESENT | PageTableFlags::WRITABLE;
+
+    let map_to_result = unsafe {
+        // FIXME: this is not safe, we do it only for testing
+        mapper.map_to(page, frame, flags, frame_allocator)
+    };
+    
+    map_to_result.expect("map_to failed").flush();
+}
+
+pub struct EmptyFrameAllocator;
+
+unsafe impl FrameAllocator<Size4KiB> for EmptyFrameAllocator {
+    fn allocate_frame(&mut self) -> Option<PhysFrame> {
+        None
+    }
 }
