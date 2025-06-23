697dc0ba87e3c97ab9051aa7a012efea4b6fcd12
devil <europedreadlydevil@gmail.com>
2025-06-24 01:49:14 +0300

    Add runner for building and running the kernel in QEMU
    This commit introduces a runner project that automates the process
    of building the kernel, creating an ISO image, and running it in QEMU.

--- runner/src/main.rs
+++ runner/src/main.rs
@@ -1,3 +1,26 @@
+use std::{env, process};
+use std::path::PathBuf;
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    let iso = env::var("ISO").unwrap();
+    println!("ISO path: {iso:?}");
+
+    let ovmf = env::var("OVMF_PATH").unwrap();
+
+    // Qemu runs our OS in a virtual
+    let mut qemu = Command::new("qemu-system-x86_64");
+
+    // Specify the path to the ISO
+    qemu.arg("-cdrom");
+    qemu.arg(env!("ISO"));
+    // For UEFI on qemu, the path to OVMF.fd is needed
+    qemu.arg("-bios").arg(PathBuf::from(ovmf).join("OVMF.fd"));
+
+    // Pass any args to qemu
+    env::args().skip(1).for_each(|arg| {
+        qemu.arg(arg);
+    });
+    let exit_status = qemu.status().unwrap();
+    process::exit(exit_status.code().unwrap_or(1));
 }
