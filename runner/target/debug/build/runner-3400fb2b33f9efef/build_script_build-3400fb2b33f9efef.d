75bccad57e00312e63aa4fac62a99a2d554a8f7e
devil <europedreadlydevil@gmail.com>
2025-06-23 21:06:27 +0300

    Clean up project

--- runner/target/debug/build/runner-3400fb2b33f9efef/build_script_build-3400fb2b33f9efef.d
+++
@@ -1,5 +0,0 @@
-/home/devil/rust projects/kernel/runner/target/debug/build/runner-3400fb2b33f9efef/build_script_build-3400fb2b33f9efef.d: build.rs
-
-/home/devil/rust projects/kernel/runner/target/debug/build/runner-3400fb2b33f9efef/build_script_build-3400fb2b33f9efef: build.rs
-
-build.rs:
