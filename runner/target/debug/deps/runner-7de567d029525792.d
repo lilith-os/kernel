c469b566d658194cbad4c3162dbbb128720eb49a
devil <europedreadlydevil@gmail.com>
2025-06-23 21:04:41 +0300

    Clean up project

---
+++ runner/target/debug/deps/runner-7de567d029525792.d
@@ -0,0 +1,5 @@
+/home/devil/rust projects/kernel/runner/target/debug/deps/runner-7de567d029525792.d: src/main.rs
+
+/home/devil/rust projects/kernel/runner/target/debug/deps/librunner-7de567d029525792.rmeta: src/main.rs
+
+src/main.rs:
---
+++ runner/target/debug/deps/runner-c413987f66f1b2ab.d
@@ -0,0 +1,5 @@
+/home/devil/rust projects/kernel/runner/target/debug/deps/runner-c413987f66f1b2ab.d: src/main.rs
+
+/home/devil/rust projects/kernel/runner/target/debug/deps/librunner-c413987f66f1b2ab.rmeta: src/main.rs
+
+src/main.rs:
