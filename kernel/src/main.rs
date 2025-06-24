3e846ea9d2234a67cef73df56cf753092e341efe
devil <europedreadlydevil@gmail.com>
2025-06-24 16:01:22 +0300

    Add tinybmp crate for bitmap support
    This commit adds the `tinybmp` crate to the kernel's dependencies,
    preparing the codebase for future bitmap image rendering features.

--- kernel/src/main.rs
+++ kernel/src/main.rs
@@ -2,13 +2,9 @@
 #![no_main]
 
 use embedded_graphics::Drawable;
-use embedded_graphics::mono_font::ascii::FONT_7X13_ITALIC;
-use embedded_graphics::mono_font::iso_8859_16::FONT_10X20;
-use embedded_graphics::mono_font::MonoTextStyle;
 use embedded_graphics::pixelcolor::Rgb888;
 use embedded_graphics::prelude::{Point, Primitive, RgbColor, Size, WebColors};
 use embedded_graphics::primitives::{Circle, PrimitiveStyle, Rectangle, Triangle};
-use embedded_graphics::text::{Alignment, Text};
 use uart_16550_driver::serial_println;
 use crate::frame_buffer::{Display};
 use crate::limine_requests::{BASE_REVISION, FRAME_BUFFER_REQUEST};
@@ -31,15 +27,6 @@ unsafe extern "C" fn _main() -> ! {
             .draw(&mut display).unwrap();
         Triangle::new(Point::new(100, 100), Point::new(200, 400), Point::new(300, 600))
             .into_styled(PrimitiveStyle::with_fill(Rgb888::CSS_AQUA))
-            .draw(&mut display).unwrap();
-        let style = MonoTextStyle::new(&FONT_10X20, Rgb888::WHITE);
-
-        Text::with_alignment(
-            "ZDAROVA SANEK)))",
-            Point::new((display.width()/2) as i32 - 10, (display.height()/2) as i32 - 20),
-            style,
-            Alignment::Center,
-        )
             .draw(&mut display).unwrap();
     }
 
